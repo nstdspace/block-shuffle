f5b221f72d8305c847eed5802246423a6f64ba23
Maksim Rotmann <maksim.rotmann@gmail.com>
2021-09-29 21:02:51 +0200

    Add bukkit logger

---
+++ src/main/java/dev/ainterr/minecraft/blockshuffle/BukkitLogger.java
@@ -0,0 +1,11 @@
+package dev.ainterr.minecraft.blockshuffle;
+
+import lombok.experimental.UtilityClass;
+import org.bukkit.Bukkit;
+
+import java.util.logging.Logger;
+
+@UtilityClass
+public class BukkitLogger {
+    public static final Logger log = Bukkit.getLogger();
+}
