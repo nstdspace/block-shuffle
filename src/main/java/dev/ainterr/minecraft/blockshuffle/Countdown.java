8abda89a701b7e27e697abce199cf0913e5abe8c
Maksim Rotmann <maksim.rotmann@gmail.com>
2021-10-01 21:54:09 +0200

    Rename variable

--- src/main/java/dev/ainterr/minecraft/blockshuffle/Countdown.java
+++ src/main/java/dev/ainterr/minecraft/blockshuffle/Countdown.java
@@ -5,18 +5,18 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class Countdown extends BukkitRunnable {
     private final BlockShufflePlugin plugin;
-    private int timeUntilEnd;
+    private int secondsUntilRoundEnd;
 
     public Countdown(BlockShufflePlugin plugin) {
         this.plugin = plugin;
-        this.timeUntilEnd = 10;
+        this.secondsUntilRoundEnd = 10;
     }
 
     @Override
     public void run() {
-        if (this.timeUntilEnd > 0) {
-            timeUntilEnd--;
-            Bukkit.broadcastMessage("BlockShuffle round over in " + timeUntilEnd + " ...");
+        if (this.secondsUntilRoundEnd > 0) {
+            secondsUntilRoundEnd--;
+            Bukkit.broadcastMessage("BlockShuffle round over in " + secondsUntilRoundEnd + " ...");
         } else {
             new Shuffle(this.plugin).runTaskLater(this.plugin, TickTimeConverterKt.TICKS_PER_SECOND);
             this.cancel();
